880e1834a5dc7e788d3cb01278ce43f55a6494f1
azarang <azarangmr@gmail.com>
2019-05-25 11:11:52 +0430

    forgot exporting some models

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export {AllActionTypes} from "./Models/ActionTypes";
+
 export {default as ReducerCreator} from "./Reducer/ReducerCreator";
 
 export {ArrayAction} from "./Models/ActionModels/ArrayModels";
@@ -5,4 +7,5 @@ export {BooleanAction} from "./Models/ActionModels/BooleanModels";
 export {DictionaryAction} from "./Models/ActionModels/DictionaryModels";
 export {ObjectAction} from "./Models/ActionModels/ObjectModels";
 export {VariableAction} from "./Models/ActionModels/VariableModels";
+export {FlagActionType} from "./Models/ActionModels/FlagModels";
 
